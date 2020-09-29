e1d03761b1712cc4689f1b8d97e9155025c69943
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2020-09-29 14:26:13 +0300

    good webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
 const path = require("path");
-
 const { merge } = require("webpack-merge");
-
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-const { plugins } = require("./postcss.config");
-
+// const { plugins } = require("./postcss.config");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-
 const WebpackBar = require("webpackbar");
-
 const loadModeConfig = (env) =>
-  require(`./build-utils/${env.mode}.config`(env));
+  require(`./build-utils/${env.mode}.config`)(env);
 
-module.exports = (env) => {
+module.exports = (env) =>
   merge(
     {
       mode: env.mode,
@@ -60,4 +54,3 @@ module.exports = (env) => {
     },
     loadModeConfig(env),
   );
-};
